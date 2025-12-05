61ded414b5f2eb7ad076dcb747cf00574df18987
Goutam Singh Sumbria <gautamsinghsumbria@gmail.com>
2025-12-06 02:10:56 +0530

    fix routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,6 +7,9 @@ import mongoose from "mongoose";
 import { connectToSocket } from "./controllers/socketManager.js";
 
 import cors from "cors";
+import userRoutes from "./routes/users.routes.js"
+
+
 
 const app = express();
 const server = createServer(app);
@@ -17,12 +20,12 @@ app.use(cors());
 app.use(express.json({ limit: "40kb" }));
 app.use(express.urlencoded({ limit: "40kb", extended: true }));
 
-
+app.use("api/v1/users",userRoutes);
 
 
 const start = async () => {
-app.set(mongo_user)
-    const connectionDb = await mongoose.connect("mongodb+srv://goutamsinghsumbria2004_db_user:MarvelAvengers@2004@cluster0.mjijlxh.mongodb.net/");
+
+    const connectionDb = await mongoose.connect("mongodb+srv://goutamsinghsumbria2004_db_user:MarvelAvengers2004@cluster0.mjijlxh.mongodb.net/");
 
     console.log(`MONGO CONNECTED DB HOST: ${connectionDb.connection.host}`)
     server.listen(app.get("port"), () => {
