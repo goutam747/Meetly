bb193a35b7bf1c90972d6e1c87346fec88e11562
Goutam Singh Sumbria <gautamsinghsumbria@gmail.com>
2025-12-12 10:18:28 +0530

    implemented socket

--- backend/src/app.js
+++ backend/src/app.js
@@ -20,7 +20,7 @@ app.use(cors());
 app.use(express.json({ limit: "40kb" }));
 app.use(express.urlencoded({ limit: "40kb", extended: true }));
 
-app.use("api/v1/users",userRoutes);
+app.use("/api/v1/users",userRoutes);
 
 
 const start = async () => {
